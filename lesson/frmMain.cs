06b2d48106f054eafcf6008467022ec6d4dcaf16
Adam Grover <adamggrover@gmail.com>
2022-12-15 20:34:31 +0000

    add text boxes for user input

--- lesson/frmMain.cs
+++ lesson/frmMain.cs
@@ -27,6 +27,21 @@ namespace lesson
             lblOutput.Text = objPerson.FName + " " + objPerson.LName;
         }
 
+        private void txtFName_TextChanged(object sender, EventArgs e)
+        {
+
+            if (this.txtbFirstName.Text != "" && this.txbLastName.Text != "")
+            {
+                btnExecute.Enabled = true;
+            }
+            else
+            {
+                btnExecute.Enabled = false;
+            }
+
+
+
+        }
         private void label1_Click(object sender, EventArgs e)
         {
 
